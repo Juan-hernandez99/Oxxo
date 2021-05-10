f4422fd0f52a2e5b39269459a632ba5ce4c473eb
Juan Blas Hernandez Aguirre <juan.aguirre.99.ja@gmail.com>
2021-05-10 15:40:02 -0500

    Creacion del proyecto

---
+++ src/main/java/mx/edu/itlapiedad/OxxoApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.itlapiedad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxxoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxxoApplication.class, args);
+	}
+
+}
